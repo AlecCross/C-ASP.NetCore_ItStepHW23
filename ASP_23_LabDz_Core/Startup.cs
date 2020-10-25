3115509fd0b2d8755cab946e2d7e99065deb9acd
AlecCross <cross2472@gmail.com>
2020-10-25 08:30:21 +0200

    Fibonacci numbers ++2

--- ASP_23_LabDz_Core/Startup.cs
+++ ASP_23_LabDz_Core/Startup.cs
@@ -38,7 +38,9 @@ namespace ASP_23_LabDz_Core
             //}
 
             //app.UseRouting();
-            //app.Map("Index", Index);//Вноситсятся элементы в проект
+
+            //Сопоставления адреса запроса с делегатом
+            //app.Map("/index", Index);//Вноситсятся элементы в проект
             //app.UseEndpoints(endpoints =>
             //{
             //    endpoints.MapGet("/", async context =>
@@ -48,34 +50,55 @@ namespace ASP_23_LabDz_Core
             //});
 
             //Элементы middleWare
-            int x = 5, y = 10, z= 0;
+            //int x = 5, y = 10, z= 0;
             //app.Use() использует делегат
             //Первый параметр обобщенного делегата имеет входной параметр,
             //Второй выходной
             //Он принимает context
-            app.Use(async(context, next) =>//Когда прийдёт входящи HTTP запрос он обработается в методе USe
+            int n = -1;
+            app.Use(async (context, next) =>//Когда прийдёт входящи HTTP запрос он обработается в методе USe
             {
-                z =x + y;
-                //После вызовется метод Invoke() который передаст управление
-                //следующему уровню middleWare app.Run в нашем случае
-                //Что такое конвеерность обработки запросов:
-                //, next второй параметр как бы запускает следующий метод middleWare внутри своего "тела"
+                n++;
+                //    //z =x + y;
+                //    //После вызовется метод Invoke() который передаст управление
+                //    //следующему уровню middleWare app.Run в нашем случае
+                //    //Что такое конвеерность обработки запросов:
+                //    //, next второй параметр как бы запускает следующий метод middleWare внутри своего "тела"
                 await next.Invoke();
-                //Не рекомендуется внутри Use описывать context.Response.WriteAsync. 
-                //Так как следующий после Use, context.Response.WriteAsync
-                //объединится со следующим через next.Invoke();
+                //    //Не рекомендуется внутри Use описывать context.Response.WriteAsync. 
+                //    //Так как следующий после Use, context.Response.WriteAsync
+                //    //объединится со следующим через next.Invoke();
 
-                //Речь идет о Content.Length он будет неверным
-               // Запись ответа должна быть только в одном месте
+                //    //Речь идет о Content.Length он будет неверным
+                //   // Запись ответа должна быть только в одном месте
+                
             });
+            
+            
             app.Run(async (context)=> //Добавляет конеченое программное обеспечение в цепь обработки запросов приложением
             {
                 //В этом контексте запроса есть доступ к параметры запроса
 
                 //await context.Response.WriteAsync($"<p>Hello world from {_enw.ApplicationName}.</p><p>Now app in {env.EnvironmentName}</p>");
-                x *= 2;
-                await context.Response.WriteAsync($"x = {x}");
+                //x *= 2;
+                //await context.Response.WriteAsync($"x = {x}");
+               
+                int a = 0;
+                int b = 1;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int temp = b;
+                    b = a + b;
+                    a = temp;
+                    
+                }
+                
+
+                await context.Response.WriteAsync($"<p>Press F5</p><p>Fibonacci number {n} = {b}</p>");
+                
             });
+            
         }
 
 
@@ -83,5 +106,39 @@ namespace ASP_23_LabDz_Core
         //{
         //    await app.
         //}
+        
+        //public ContentResult GetFibanacci(int n)
+        //{
+        //    int a = 0;
+        //    int b = 1;
+
+        //    for (int i = 0; i < n - 1; i++)
+        //    {
+        //        int temp = b;
+        //        b = a + b;
+        //        a = temp;
+        //    }
+
+        //    return Content($"<p>Фибаначи числа {n} = {b}</p>");
+        //}
+
+        //public int fib(int n)
+        //{
+        //    if (n == 0)
+        //    {
+        //        return 0;
+        //    }
+        //    else if (n == 1)
+        //    {
+        //        return 1;
+        //    }
+
+        //    return fib(n - 1) + fib(n - 2);
+        //}
+        //public ContentResult GetFibanacciR(int n)
+        //{
+        //    return Content($"<p>Фибаначи числа {n} = {fib(n)}</p>");
+        //}
+
     }
 }
